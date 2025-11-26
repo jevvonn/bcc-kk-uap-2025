eae8b0f237fc73f4b34b4407a94edeaa2188f8f5
jevvonn <darmawanb985@gmail.com>
2025-11-26 17:25:29 +0700

    add initial implementation of Lagu class and main program structure

---
+++ Solution.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+class Lagu {
+    // --- Atribut private --- (Don't change this)
+    private String nama;
+    private String penulis;
+    private int durasi;
+
+    // --- Constructor ---
+    public Lagu(String nama, String penulis, int durasi) {
+        // TODO: isi constructor
+    }
+
+    public String getNama() {
+        // TODO
+        return null;
+    }
+
+    public String getPenulis() {
+        // TODO
+        return null;
+    }
+
+    public int getDurasi() {
+        // TODO
+        return 0;
+    }
+
+    public void setNama(String nama) {
+        // TODO
+    }
+
+    public void setPenulis(String penulis) {
+        // TODO
+    }
+
+    public void setDurasi(int durasi) {
+        // TODO
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt(); // jumlah maksimal lagu
+        Lagu[] playlist = new Lagu[N];
+
+        // TODO: implementasikan logika utama program sesuai dengan deskripsi soal
+
+    }
+}
